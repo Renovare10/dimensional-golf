4551a29352b6b3f70cb9b60285f8c6caad5f2149
Chadfire <chadfire10@yahoo.com>
2026-03-20 20:50:02 -0400

    Started on movement

---
+++ ui/main_menu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _on_btn_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://levels/LevelMenu.tscn")
+
+
+func _on_btn_quit_pressed() -> void:
+	get_tree().quit()
